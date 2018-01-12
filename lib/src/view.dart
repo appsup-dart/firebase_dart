4187d9f71f6c2df91e665547da801863eeafa9f8
rbellens <rik.bellens@telin.ugent.be>
2018-01-12 19:16:18 +0100

    fix 'The method 'operationForChild' was called on null.'

--- lib/src/view.dart
+++ lib/src/view.dart
@@ -33,12 +33,17 @@ class ViewCache {
       );
 
   ViewCache child(Name c) {
+    var childPendingOperations = new SortedMap();
+    for (var k in pendingOperations.keys) {
+      var o = pendingOperations[k].operationForChild(c);
+      if (o!=null) {
+        childPendingOperations[k] = o;
+      }
+    }
     var v = new ViewCache(
       localVersion.child(c),
       serverVersion.child(c),
-      new SortedMap.fromIterables(pendingOperations.keys,
-          pendingOperations.values
-              .map((o)=>o.operationForChild(c))),
+      childPendingOperations,
     );
     return v;
   }
