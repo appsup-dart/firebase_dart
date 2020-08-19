244e6a93de6b01cac7497a9a0c889a330b6b5da1
rbellens <rik.bellens@appsup.be>
2020-08-19 18:00:45 +0200

    setup with storagePath argument

--- lib/implementation/pure_dart.dart
+++ lib/implementation/pure_dart.dart
@@ -1,12 +1,11 @@
-import 'dart:io';
-
 import 'package:firebase_dart/src/implementation.dart';
 import 'package:firebase_dart/src/implementation/dart.dart';
 import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
 
 class PureDartFirebase {
-  static void setup() {
-    Hive.init(Directory.systemTemp.path);
+  static void setup({@required String storagePath}) {
+    if (storagePath != null) Hive.init(storagePath);
     FirebaseImplementation.install(PureDartFirebaseImplementation());
   }
 }
