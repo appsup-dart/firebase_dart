15c07ac1edf74a53eaaaa931627094bbd3213a76
rbellens <rik.bellens@appsup.be>
2021-01-06 13:22:35 +0100

    test for obsolete treestructureddata instances

--- test/database/synctree_test.dart
+++ test/database/synctree_test.dart
@@ -1,4 +1,7 @@
+import 'package:collection/collection.dart';
+import 'package:firebase_dart/src/database/impl/data_observer.dart';
 import 'package:firebase_dart/src/database/impl/operations/tree.dart';
+import 'package:firebase_dart/src/database/impl/persistence/manager.dart';
 import 'package:firebase_dart/src/database/impl/synctree.dart';
 import 'package:firebase_dart/src/database/impl/tree.dart';
 import 'package:firebase_dart/src/database/impl/treestructureddata.dart';
@@ -70,5 +73,84 @@ void main() {
             false);
       });
     }, skip: 'Completeness on user operation is not handled correctly yet');
+
+    group('Performance measures', () {
+      test('Obsolete TreeStrucutedData instances', () {
+        var syncTree =
+            SyncTree('test:///', persistenceManager: NoopPersistenceManager());
+
+        syncTree.addEventListener(
+            'value', Name.parsePath('main'), QueryFilter(), (event) {});
+        for (var i = 0; i < 10; i++) {
+          syncTree.addEventListener(
+              'value', Name.parsePath('main/$i'), QueryFilter(), (event) {});
+        }
+        syncTree.applyServerOperation(
+            TreeOperation.overwrite(
+                Name.parsePath('main'),
+                TreeStructuredData.fromJson({
+                  for (var i = 0; i < 10; i++) '$i': {'value': i},
+                })),
+            QueryFilter());
+        for (var i = 0; i < 10; i++) {
+          syncTree.applyServerOperation(
+              TreeOperation.overwrite(Name.parsePath('main/$i/valueX2'),
+                  TreeStructuredData.fromJson(i)),
+              QueryFilter());
+        }
+
+        print(
+            'obsolete TreeStructuredData instances = ${syncTree.obsoleteTreeStructuredDataInstanceCount}');
+
+        expect(syncTree.obsoleteTreeStructuredDataInstanceCount, 0,
+            skip: 'TODO improve reuse of instances');
+      });
+    });
   });
+}
+
+extension SyncTreeMeasurer on SyncTree {
+  int get obsoleteTreeStructuredDataInstanceCount {
+    var root = TreeNode<Name, Set<TreeStructuredData>>();
+
+    void handleOperation(
+        TreeNode<Name, Set<TreeStructuredData>> tree, Operation operation) {
+      if (operation is TreeOperation) {
+        handleOperation(
+            tree.subtree(operation.path, (parent, name) => TreeNode()),
+            operation.nodeOperation);
+      } else if (operation is Overwrite) {
+        var set = tree.value ??= EqualitySet(IdentityEquality());
+        set.add(operation.value);
+        operation.value.children.forEach((key, value) {
+          handleOperation(
+              tree, TreeOperation.overwrite(Path.from([key]), value));
+        });
+      } else if (operation is Merge) {
+        for (var o in operation.overwrites) {
+          handleOperation(tree, o);
+        }
+      }
+    }
+
+    this.root.forEachNode((key, value) {
+      var tree = root.subtree(key, (parent, name) => TreeNode());
+      for (var view in value.views.values) {
+        for (var data in [view.data.localVersion, view.data.serverVersion]) {
+          var op = data.toOperation();
+          handleOperation(tree, op);
+        }
+      }
+    });
+
+    var obsoleteCount = 0;
+    root.forEachNode((key, value) {
+      if (value == null) return;
+      value.removeWhere((element) => element == TreeStructuredData());
+
+      obsoleteCount += value.length - Set.from(value).length;
+    });
+
+    return obsoleteCount;
+  }
 }
