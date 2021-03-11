58fe44c5997e84f74af77f78bfde41381617e97b
rikbellens <rik.bellens@appsup.be>
2021-03-11 15:49:33 +0100

    null safety main database file

--- packages/firebase_dart/lib/database.dart
+++ packages/firebase_dart/lib/database.dart
@@ -1,10 +1,6 @@
 // Copyright (c) 2016, Rik Bellens. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
-/// A pure Dart implementation of the Firebase client
-///
 library firebase_database;
 
 export 'src/database.dart';
