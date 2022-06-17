cc2dd8a707d96882795f8b838fc55ff4fcf966f3
rikbellens <rik.bellens@appsup.be>
2022-06-17 10:58:51 +0200

    accept custom ssl certificates in debug mode in example app

--- packages/firebase_dart_flutter/example/lib/main.dart
+++ packages/firebase_dart_flutter/example/lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+
 import 'package:firebase_dart_flutter/firebase_dart_flutter.dart';
 import 'package:firebase_dart_flutter_example/src/core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  // Accept custom ssl certificates for use in proxy servers to be able to
+  // capture/monitor network traffic for debugging purposes
+  if (kDebugMode) HttpOverrides.global = MyHttpOverrides();
+
   await FirebaseDartFlutter.setup(isolated: false);
   runApp(const MyApp());
 }
@@ -15,5 +22,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: AppListPage(),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+        return true;
+      };
   }
 }
