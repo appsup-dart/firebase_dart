07033e52ed664a8a03d5f75a5606f0c013c19212
J.C.Ködel <jckodel@gmail.com>
2025-07-24 22:59:02 -0300

    Fixes for #62 and #63

--- packages/firebase_dart/lib/src/core/impl/persistence.dart
+++ packages/firebase_dart/lib/src/core/impl/persistence.dart
@@ -4,14 +4,31 @@ import 'package:hive/hive.dart';
 
 class PersistenceStorage {
   static bool _memoryStorage = false;
+
+  static final _openBoxes = <String, Box>{};
+
   static Future<Box> openBox(String name) async {
+    final openBox = _openBoxes[name];
+
+    if (openBox != null) return openBox;
+
+    Future<Box> getBox() async {
+      final box = await Hive.openBox(
+        name,
+        bytes: _memoryStorage ? Uint8List(0) : null,
+      );
+
+      await box.compact();
+
+      return _openBoxes[name] = box;
+    }
+
     try {
-      return await Hive.openBox(name,
-          bytes: _memoryStorage ? Uint8List(0) : null);
+      return await getBox();
     } on HiveError {
       await Hive.deleteBoxFromDisk(name);
-      return await Hive.openBox(name,
-          bytes: _memoryStorage ? Uint8List(0) : null);
+
+      return await getBox();
     }
   }
 
