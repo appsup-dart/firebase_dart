ffe6ed6211f48bd07d355127d01a458234bdc86b
rikbellens <rik.bellens@appsup.be>
2025-09-17 10:41:11 +0200

    refactor: support for package_info_plus ^9.0.0

--- packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,22 +7,22 @@ import Foundation
 
 import app_links
 import facebook_auth_desktop
-import flutter_inappwebview_macos
 import flutter_secure_storage_macos
 import google_sign_in_ios
 import package_info_plus
 import path_provider_foundation
 import sign_in_with_apple
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
-  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
